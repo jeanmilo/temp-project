c92a782d753ccdc332f737e8bf7c59d4c9a16e3a
Claudia <claudia.parlee@gmail.com>
2025-03-31 12:31:43 -0400

    helllooo

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: "imherelookhere", //this line will tell the build to create a file with this name
 };
 
 export default nextConfig;
